baca630845cdc6e7f2b500034d7802a9f5b3ace5
Kourosh Creat1ve <zahdnia.korosh@gmail.com>
2024-11-12 19:45:12 +0330

    Clean

--- ApplicationPhoneBook/Services/GetlistContact/ContactListDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApplicationPhoneBook.Services.GetlistContact
-{
-    public class ContactListDto
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
